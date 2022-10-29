f07ade822784bd15a28fd46eb216e8d46fe1798d
RemoteNet <remotenet@remotenet.com>
2022-10-29 17:59:49 +0300

    [Trace] Implement custom handlers

--- rnet-trace/TraceContext.cs
+++ rnet-trace/TraceContext.cs
@@ -1,29 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace remotenet_trace
+namespace RemotenetTrace
 {
     internal class TraceContext
     {
+        public class MethodParameter
+        {
+            public string Type { get; set; }
+            public string Name { get; set; }
+
+            public MethodParameter(string type, string name)
+            {
+                Type = type;
+                Name = name;
+            }
+
+            public override string ToString() => $"{Type} {Name}";
+        }
+
         public string StackTrace { get; private set; }
         public DateTime StartTime { get; private set; }
         public string ClassName { get; private set; }
         public string MethodName { get; private set; }
-        public string[] ParamNames { get; private set; }
-        public string[] ParamTypes { get; private set; }
+        public MethodParameter[] Parameters { get; private set; }
 
-        public TraceContext(string stackTrace, DateTime startTime, string className, string methodName, string[] paramNames, string[] paramTypes)
+        public TraceContext(string stackTrace, DateTime startTime, string className, string methodName, string[] paramTypes, string[] paramNames)
         {
             StackTrace = stackTrace;
             StartTime = startTime;
             ClassName = className;
             MethodName = methodName;
-            ParamNames = paramNames;
-            ParamTypes = paramTypes;
+            Parameters = paramTypes.Zip(paramNames).Select(tuple => new MethodParameter(tuple.First, tuple.Second)).ToArray();
         }
+
+        public string PrettyParametersList() => String.Join(", ", Parameters.Select(p => p.ToString()));
 
         public Dictionary<string, object> ToDictionary()
         {
