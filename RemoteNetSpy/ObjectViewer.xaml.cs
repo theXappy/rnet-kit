c9ea8e76b89bed2cbcd54511f4c3186cd057c608
Xappy <noforu@gmail.com>
2022-12-03 23:23:22 +0200

    [ObjectViewer] Recursive viewing, refactor code
    Merge logic for Fields and Props as much as possible

--- RemoteNetSpy/ObjectViewer.xaml.cs
+++ RemoteNetSpy/ObjectViewer.xaml.cs
@@ -4,12 +4,14 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -29,8 +31,9 @@ namespace RemoteNetSpy
         public ObjectViewer(Window parent, RemoteObject ro)
         {
             InitializeComponent();
-            this.Height = parent.Height * 0.9;
-            this.Width = parent.Width * 0.9;
+            double multiplier = parent is ObjectViewer ? 1 : 0.9;
+            this.Height = parent.Height * multiplier;
+            this.Width = parent.Width * multiplier;
 
             _ro = ro;
             _type = _ro.GetType();
@@ -41,60 +44,30 @@ namespace RemoteNetSpy
             DynamicRemoteObject dro = _ro.Dynamify() as DynamicRemoteObject;
 
             ObservableCollection<MembersGridItem> items = new ObservableCollection<MembersGridItem>();
-            foreach (FieldInfo field in _type.GetFields((BindingFlags)0xffff))
+
+            IEnumerable<MemberInfo> fields = _type.GetFields((BindingFlags)0xffff);
+            IEnumerable<MemberInfo> props = _type.GetProperties((BindingFlags)0xffff);
+
+            foreach (MemberInfo member in fields.Concat(props).OrderBy(m => m.Name))
             {
                 MembersGridItem mgi = new MembersGridItem()
                 {
-                    MemberType = "Field",
-                    Name = field.Name,
-                    Type = field.FieldType.ToString() // Specifying expected type
+                    Name = member.Name,
                 };
-                if (DynamicRemoteObject.TryGetDynamicMember(dro, field.Name, out dynamic res))
+                if (member is FieldInfo fi)
                 {
-                    mgi.Value = res.ToString();
-                    string acutalType = res.GetType().FullName;
-                    if (mgi.Type != acutalType)
-                    {
-                        mgi.Type += "\t{" + acutalType + '}'; // Specifying actual type if it's different (might be a subclass)
-                    }
+                    mgi.MemberType = "Field";
+                    mgi.Type = NormalizeGenericTypeFullName(fi.FieldType.ToString()); // Specifying expected type
                 }
-                else
+                if (member is PropertyInfo pi)
                 {
-                    mgi.Value = "ERROR: Couldn't read value";
+                    mgi.MemberType = "Property";
+                    mgi.Type = NormalizeGenericTypeFullName(pi.PropertyType.ToString()); // Specifying expected type
                 }
-                items.Add(mgi);
-            }
-            foreach (PropertyInfo field in _type.GetProperties((BindingFlags)0xffff))
-            {
-                MembersGridItem mgi = new MembersGridItem()
-                {
-                    MemberType = "Property",
-                    Name = field.Name,
-                    Type = field.PropertyType.ToString() // Specifying expected type
-                };
+
                 try
                 {
-                    if (DynamicRemoteObject.TryGetDynamicMember(dro, field.Name, out dynamic res))
-                    {
-                        if (res == null)
-                        {
-                            mgi.Value = "null";
-                            continue;
-                        }
-
-                        mgi.Value = res.ToString();
-                        string acutalType = res.GetType().FullName;
-                        if (mgi.Type != acutalType)
-                        {
-                            // Specifying actual type if it's different (might be a subclass)
-                            mgi.Type += " {" + acutalType +
-                                        '}';
-                        }
-                    }
-                    else
-                    {
-                        mgi.Value = "ERROR: Couldn't read value";
-                    }
+                    GetMemberValue(dro, member, mgi);
                 }
                 catch (Exception ex)
                 {
@@ -103,16 +76,118 @@ namespace RemoteNetSpy
 
                 items.Add(mgi);
             }
-
+           
             membersGrid.ItemsSource = items;
+        }
+
+        private static void GetMemberValue(DynamicRemoteObject? dro, MemberInfo field, MembersGridItem mgi)
+        {
+            if (DynamicRemoteObject.TryGetDynamicMember(dro, field.Name, out dynamic res))
+            {
+                mgi.RawValue = res;
+                if (res != null)
+                {
+                    mgi.Value = res.ToString();
+                    string acutalType = res.GetType().FullName;
+                    if (mgi.Type != acutalType)
+                    {
+                        // Specifying actual type if it's different (might be a subclass)
+                        mgi.Type += " {" + NormalizeGenericTypeFullName(acutalType) +
+                                    '}';
+                    }
+
+                    // TODO: Ugly hack...
+                    if (mgi.Type == "System.Byte[]")
+                    {
+                        byte[] LocalBytes = (res as DynamicRemoteObject).Cast<byte>().ToArray();
+                        mgi.Value += " { ";
+                        bool first = true;
+                        foreach (byte localByte in LocalBytes)
+                        {
+                            if (!first)
+                                mgi.Value += ", ";
+                            first = false;
+                            mgi.Value += "0x";
+                            mgi.Value += localByte.ToString("X2");
+                        }
+                        mgi.Value += " }";
+                    }
+                }
+                else
+                {
+                    mgi.Value = "null";
+                }
+            }
+            else
+            {
+                mgi.Value = "ERROR: Couldn't read value";
+            }
         }
 
         public class MembersGridItem
         {
+            public object RawValue { get; set; }
+
             public string MemberType { get; set; }
             public string Name { get; set; }
             public string Value { get; set; }
             public string Type { get; set; }
+        }
+
+        private void CloseButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void InspectClicked(object sender, RoutedEventArgs e)
+        {
+            MembersGridItem mgi = (sender as Button)?.DataContext as MembersGridItem;
+            if (mgi == null)
+                return;
+            if(mgi.RawValue == null)
+                return;
+            RemoteObject? ro = mgi.RawValue as RemoteObject;
+            if (mgi.RawValue is not RemoteObject)
+                ro = (mgi.RawValue as DynamicRemoteObject)?.__ro;
+
+            if (ro == null)
+            {
+                MessageBox.Show("Value is not a Remote Object.\nHere's a ToString():\n" + ro);
+            }
+
+            (new ObjectViewer(this, ro)).ShowDialog();
+        }
+
+        private void UIElement_OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var parent = (sender as TextBlock).Parent as Grid;
+            TextBox? tBox = parent?.Children.Cast<UIElement>().Single(c => c is TextBox) as TextBox;
+            if(tBox == null)
+                return;
+            tBox.Visibility = Visibility.Visible;
+            tBox.Focus();
+        }
+
+        private static string NormalizeGenericTypeFullName(string fullName)
+        {
+            // Use a regular expression to match and replace the extra type information
+            string parsedFullName = Regex.Replace(fullName, @",\s*[^,]+\s*,\s*Version=\d+\.\d+\.\d+\.\d+\s*,\s*Culture=\w+\s*,\s*PublicKeyToken=\w+", "");
+
+            // Use another regular expression to match and replace the square brackets
+            parsedFullName = parsedFullName.Replace("[[", "<");
+            parsedFullName = parsedFullName.Replace("]]", ">");
+            parsedFullName = Regex.Replace(parsedFullName, @"<([^<>]*)\],\[([^<>]*)>", "<$1, $2>");
+
+            // Use another regular expression to match and remove the backtick and number after it
+            parsedFullName = Regex.Replace(parsedFullName, @"\`\d", "");
+
+            // Output the parsed full name of the generic type, which should be "System.Collections.Generic.List<System.Threading.Tasks.Task<System.Collections.Generic.Dictionary<System.Int32, System.String>>>"
+            return parsedFullName;
+        }
+
+        private void UIElement_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            (sender as TextBox).Visibility = Visibility.Hidden;
         }
     }
 }
