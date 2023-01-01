b48711c19a92cae4943c9d888ce81cb7b293ee5e
RemoteNet <remotenet@remotenet.com>
2023-01-01 22:32:13 +0200

    [Spy] Add IEnumerable  support in Object Viewer

--- RemoteNetSpy/MemberToIconConverter.cs
+++ RemoteNetSpy/MemberToIconConverter.cs
@@ -101,6 +101,19 @@ public class BoolToVisabilityConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+public class InverseBoolToVisabilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 
 public class DumpedTypeToDescription : IValueConverter
---
+++ RemoteNetSpy/StrValueToVisibilityConverter.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using RemoteNET;
+using RemoteNET.Internal;
+
+namespace RemoteNetGui;
+
+public class StrValueToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return Visibility.Collapsed;
+
+        if(value is RemoteObject || value is DynamicRemoteObject)
+            return Visibility.Visible;
+        if (value.GetType().IsArray)
+            return Visibility.Visible;
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class RawViewItemToVisabilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return Visibility.Collapsed;
+
+        if (value is string str)
+            return str.StartsWith("Raw View[") ? Visibility.Visible : Visibility.Collapsed;
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class RawViewToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return Visibility.Collapsed;
+
+        if (value is string str)
+            return str == "Raw View" ? Visibility.Visible : Visibility.Collapsed;
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
