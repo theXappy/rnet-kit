87bf18a7085cfd13048b1187d4aa1785292c4e0b
RemoteNet <remotenet@remotenet.com>
2023-09-15 11:59:04 +0300

    [dump] Rearrange code into classes

---
+++ rnet-dump/TypesDumpOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+[Verb("types", HelpText = "Dump all types matching a query")]
+public class TypesDumpOptions
+{
+    [Option('t', "target", Required = true, HelpText = "Target process name. Partial names are allowed but a single match is expected. " +
+                                                       "e.g. \"notep\" for notepad")]
+    public string? TargetProcess { get; set; }
+    [Option('q', "type-query", Required = true, HelpText = "Query for the Full Name of the types to find")]
+    public string Query { get; set; }
+    [Option('u', "unmanaged", Required = false, HelpText = "Whether the target is an native app")]
+    public bool Unmanaged { get; set; }
+}
---
+++ rnet-dump/TypesDumper.cs
@@ -0,0 +1,56 @@
+﻿using RemoteNET;
+using System.Text;
+
+namespace remotenet_dump;
+
+public static class TypesDumper
+{
+    public static int DumpTypes(TypesDumpOptions opts)
+    {
+        bool IsReasonableUnmanagedTypeName(string str)
+        {
+            if (str.Length < 2)
+                return false;
+            bool valid = !str.Any(Char.IsControl);
+            if (!valid)
+                return false;
+            valid = !str.Any(c => c > 0xff || c == '\r' || c == '\n' || c == '"');
+            if (!valid)
+                return false;
+            return valid;
+        }
+
+        Console.WriteLine("Loading...");
+        List<CandidateType>? candidates = null;
+        try
+        {
+            using RemoteApp app = Common.Connect(opts.TargetProcess, opts.Unmanaged);
+            candidates = app.QueryTypes(opts.Query).ToList();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("ERROR: " + e);
+            return 1;
+        }
+
+        if (candidates == null || !candidates.Any())
+        {
+            Console.WriteLine("ERROR: Failed to find any remote type for the given query");
+            return 1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Console.WriteLine($"Matches:");
+        foreach (var type in candidates)
+        {
+            // Skip types with unreasonable names. Our unmanaged types discovery logic is *very* permissive.
+            if (type.Runtime == RuntimeType.Unmanaged && !IsReasonableUnmanagedTypeName(type.TypeFullName))
+                continue;
+
+            sb.AppendLine($"[{type.Runtime}][{type.Assembly}] {type.TypeFullName}");
+        }
+
+        Console.WriteLine(sb.ToString());
+        return 0;
+    }
+}
