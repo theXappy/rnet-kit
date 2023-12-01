9d68efe76e58722233e95d64090676afe19165d8
Xappy <noforu@gmail.com>
2023-12-01 11:12:11 +0200

    [Trace] Share Remote App with hook scripts

--- rnet-trace/HandlerGlobals.cs
+++ rnet-trace/HandlerGlobals.cs
@@ -1,9 +1,11 @@
 ﻿using System.Text;
+using RemoteNET;
 
 namespace RemotenetTrace;
 
 public class HandlerGlobals
 {
+    public RemoteApp App { get; set; }
     public TraceContext Context { get; set; }
     public dynamic Instance { get; set; }
     public dynamic[] Args { get; set; }
