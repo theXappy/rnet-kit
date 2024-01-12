225b9c5015c41a038a48edf72d954cf89b309b6f
Xappy <noforu@gmail.com>
2024-01-12 15:32:30 +0200

    [dump] Dump domains

---
+++ rnet-dump/DomainsDumpOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+[Verb("domains", HelpText = "Dump all domains and list modules within")]
+public class DomainsDumpOptions
+{
+    [Option('t', "target", Required = true, HelpText = "Target process name. Partial names are allowed but a single match is expected. " +
+                                                       "e.g. \"notep\" for notepad")]
+    public string? TargetProcess { get; set; }
+    [Option('u', "unmanaged", Required = false, HelpText = "Whether the target is an native app")]
+    public bool Unmanaged { get; set; }
+}
--- rnet-dump/Program.cs
+++ rnet-dump/Program.cs
@@ -5,10 +5,11 @@ using remotenet_dump;
 public class Program
 {
     public static int Main(string[] args) =>
-    Parser.Default.ParseArguments<TypesDumpOptions, HeapOptions, MembersDumpOptions>(args)
+    Parser.Default.ParseArguments<TypesDumpOptions, HeapOptions, MembersDumpOptions, DomainsDumpOptions>(args)
         .MapResult(
         (HeapOptions options) => HeapDumper.DumpHeap(options),
         (TypesDumpOptions options) => TypesDumper.DumpTypes(options),
         (MembersDumpOptions options) => MembersDumper.DumpMembers(options),
+        (DomainsDumpOptions options) => DomainsDumper.DumpDomains(options),
         errors => 1);
 }
