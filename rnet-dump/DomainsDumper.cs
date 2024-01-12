75d0a53d9de7c090c0d32b74613f2a2d62bc9e3b
RemoteNet <remotenet@remotenet.com>
2024-01-12 15:32:30 +0200

    [dump] Dump domains

---
+++ rnet-dump/DomainsDumper.cs
@@ -0,0 +1,49 @@
+﻿using RemoteNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScubaDiver.API.Interactions.Dumps;
+
+namespace remotenet_dump
+{
+    internal class DomainsDumper
+    {
+        public static int DumpDomains(DomainsDumpOptions opts)
+        {
+            Console.WriteLine("Loading...");
+            DomainsDump? domains = null;
+            try
+            {
+                using RemoteApp app = Common.Connect(opts.TargetProcess, opts.Unmanaged);
+                domains = app.Communicator.DumpDomains();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e);
+                return 1;
+            }
+
+            if (domains == null || !domains.AvailableDomains.Any())
+            {
+                Console.WriteLine("ERROR: Failed to find any domains for the given query");
+                return 1;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine($"Matches:");
+            foreach (DomainsDump.AvailableDomain domain in domains.AvailableDomains)
+            {
+                sb.AppendLine($"[domain] {domain.Name}");
+                foreach (string module in domain.AvailableModules)
+                {
+                    sb.AppendLine($"[module] {module}");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+            return 0;
+        }
+    }
+}
