12d7efe92b78e617949a3db20a7e76ea124f745a
RemoteNet <remotenet@remotenet.com>
2024-05-30 23:11:13 +0300

    [Spy] Fix icons in Object Viewer

--- RemoteNetSpy/DumpedMember.cs
+++ RemoteNetSpy/DumpedMember.cs
@@ -2,7 +2,7 @@ namespace RemoteNetSpy;
 
 public class DumpedMember
 {
-    public string MemberType => RawName.Substring(0, RawName.IndexOf(']') + 1);
+    public string MemberType => RawName.Substring(1, RawName.IndexOf(']') - 1);
     public string RawName { get; set; }
     // This one has generic args normalized from [[System.Byte, ... ]] to <System.Byte>
     public string NormalizedName { get; set; }
