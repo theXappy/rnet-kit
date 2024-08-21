bf324b4774e0917d44442cf25495f872d0b3304a
Xappy <noforu@gmail.com>
2024-08-21 22:28:42 +0300

    [Spy] Choose diver (man/unman) dialog

---
+++ RemoteNetSpy/DiverSelectionDialog.xaml.cs
@@ -0,0 +1,43 @@
+﻿using RemoteNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RemoteNetSpy
+{
+    /// <summary>
+    /// Interaction logic for DiverSelectionDialog.xaml
+    /// </summary>
+    public partial class DiverSelectionDialog : Window
+    {
+        public RuntimeType SelectedRuntime { get; set; }
+
+        public DiverSelectionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ManagedButtonClicked(object sender, RoutedEventArgs e)
+        {
+            SelectedRuntime = RuntimeType.Managed;
+            this.DialogResult = true;
+            this.Close();
+        }
+        private void UnmanagedButtonClicked(object sender, RoutedEventArgs e)
+        {
+            SelectedRuntime = RuntimeType.Unmanaged;
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
