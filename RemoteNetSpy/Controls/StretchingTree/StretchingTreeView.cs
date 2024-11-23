15074fbba480c980215656779a4414f1394d318a
RemoteNet <remotenet@remotenet.com>
2024-11-23 23:40:37 +0200

    [Spy] Stretch tree view items

---
+++ RemoteNetSpy/Controls/StretchingTree/StretchingTreeView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RemoteNetSpy.Controls.StretchingTree
+{
+    public class StretchingTreeView : TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new StretchingTreeViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is StretchingTreeViewItem;
+        }
+    }
+}
