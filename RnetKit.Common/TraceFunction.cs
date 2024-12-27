3ebb978bf0618a070f12556777ed26843a3cf1c5
RemoteNet <remotenet@remotenet.com>
2024-12-27 16:38:20 +0200

    [Spy] fix tracing multiple methods

--- RnetKit.Common/TraceFunction.cs
+++ RnetKit.Common/TraceFunction.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 
 namespace RnetKit.Common
 {
-    public class TraceFunction
+    public class TraceFunction : IComparable
     {
         public string DemangledName { get; set; }
         public string FullMangledName { get; set; }
@@ -32,6 +32,17 @@ namespace RnetKit.Common
         {
             string json = File.ReadAllText(filePath);
             return FromJson(json);
+        }
+
+        // IComparable Impl
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            TraceFunction otherFunction = obj as TraceFunction;
+            if (otherFunction != null)
+                return this.DemangledName.CompareTo(otherFunction.DemangledName);
+            else
+                throw new ArgumentException("Object is not a TraceFunction");
         }
     }
 }
