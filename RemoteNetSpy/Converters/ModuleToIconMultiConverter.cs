c241e2e93571aba1b9e91e87f44f5b463445a821
RemoteNet <remotenet@remotenet.com>
2025-03-08 12:50:42 +0200

    [Spy] Module Icon converter refactor
    Make it a "multi value" one so we can bind to 2 properties instead of the "Assembly" obj itself

---
+++ RemoteNetSpy/Converters/ModuleToIconMultiConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using RemoteNET;
+
+namespace RemoteNetSpy.Converters;
+
+public class ModuleToIconMultiConverter : IMultiValueConverter
+{
+    private static readonly BitmapImage CppProjectNodeIcon;
+    private static readonly BitmapImage CppProjectNodeGrayedIcon;
+    private static readonly BitmapImage ModulePublicIcon;
+    private static readonly BitmapImage ModulePublicGrayedIcon;
+
+    static ModuleToIconMultiConverter()
+    {
+        CppProjectNodeIcon = new BitmapImage(new Uri("/icons/CPPProjectNode.png", UriKind.RelativeOrAbsolute));
+        CppProjectNodeGrayedIcon = new BitmapImage(new Uri("/icons/CPPProjectNode_grayed.png", UriKind.RelativeOrAbsolute));
+        ModulePublicIcon = new BitmapImage(new Uri("/icons/ModulePublic.png", UriKind.RelativeOrAbsolute));
+        ModulePublicGrayedIcon = new BitmapImage(new Uri("/icons/ModulePublic_grayed.png", UriKind.RelativeOrAbsolute));
+    }
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length < 2) return null;
+
+        RuntimeType runtime = (RuntimeType)values[0];
+        bool anyTypes = (int)values[1] > 0; // AnyTypes
+
+        if (runtime == RuntimeType.Unmanaged)
+        {
+            return anyTypes ? CppProjectNodeIcon : CppProjectNodeGrayedIcon;
+        }
+        else
+        {
+            return anyTypes ? ModulePublicIcon : ModulePublicGrayedIcon;
+        }
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
