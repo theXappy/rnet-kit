162d8719fcc2729a297f29ce8c1b43e95ced33dc
RemoteNet <remotenet@remotenet.com>
2026-01-31 18:13:09 +0200

    [rnet-vessel] Add new project

---
+++ rnet-vessel/RnetVesselProgram.cs
@@ -0,0 +1,8 @@
+﻿namespace rnet_vessel
+{
+    public class RnetVesselProgram
+    {
+        public static void Main(string[] args) =>
+            RemoteNET.Vessel.VesselProgram.Main(args);
+    }
+}
